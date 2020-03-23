7fbb47d9ce09b017e35b8d3881fc445a993e6db2
IgorLIS <lapshin.is84@gmail.com>
2020-03-23 20:50:57 +0300

    color

---
+++ datanum/lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyWig());
+
+class MyWig extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Ferst wig',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: OneHomePage(),
+    );
+  }
+}
+
+class OneHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(
+          Icons.undo,
+          color: Colors.black,
+        ),
+        title: Center(
+          child: Text(
+            'one programm',
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.mail),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Center(
+        child: Container(
+          height: 350,
+          width: 350,
+          
+          decoration: new BoxDecoration(
+    boxShadow: [
+      BoxShadow(
+        color: Colors.blueGrey,
+        blurRadius: 20.0, // has the effect of softening the shadow
+        spreadRadius: 5.0, // has the effect of extending the shadow
+        offset: Offset(
+          10.0, // horizontal, move right 10
+          10.0, // vertical, move down 10
+        ),
+      )
+    ],
+    
+  ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Text(
+                'Авторизация',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Password'),
+                ),
+              ),
+            ),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(8.0),
+                  splashColor: Colors.blueAccent,
+                  onPressed: () {},
+                  child: Text(
+                    'Войти',
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),),
+      ),
+    );
+  }
+}
