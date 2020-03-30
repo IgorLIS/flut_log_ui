5c222ab296ab2972fa7b3eaf24edac8331802339
IgorLIS <lapshin.is84@gmail.com>
2020-03-30 17:07:19 +0300

    BoxDecor

--- hi/lib/main.dart
+++ hi/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main()=> runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
       title: 'fist programm',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        
         primaryColor: Colors.pink[100],
       ),
       home: OnePage(),
@@ -24,9 +25,18 @@ class OnePage extends StatelessWidget {
         leading: Icon(Icons.menu),
         title: Text('Приложение для тестов'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.mic, color: Colors.pink,), onPressed: null),
-          IconButton(icon: Icon(Icons.settings, color: Colors.pink,), onPressed: null),
-          
+          IconButton(
+              icon: Icon(
+                Icons.mic,
+                color: Colors.pink,
+              ),
+              onPressed: null),
+          IconButton(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.pink,
+              ),
+              onPressed: null),
         ],
       ),
       body: Column(
@@ -35,53 +45,95 @@ class OnePage extends StatelessWidget {
             padding: EdgeInsets.all(25.0),
             color: Colors.pink[50],
             child: Center(
-                child: 
-              Container(
+              child: Container(
+                width: 400,
+                height: 550,
+                color: Colors.pink[300],
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.teal,
+                          border: Border.all(width:2.0),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Кубик первый',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                        )),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ClipRRect(
+                      child: Container(
+                        width: 200,
+                        height: 100,
+                        color: Colors.yellow[200],
+                        child: Center(
+                          child: Text(
+                            'Кубик второй',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 30, fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      '5',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 90,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                // decoration: BoxDecoration(
+                //   border: Border.all(
+                //     color: Colors.teal,
+                //     width: 10,
+                //   ),
+                //   // borderRadius:BorderRadius.circular(12),
+                // ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          ClipRRect(
+            child: Container(
               width: 400,
-              height: 550,
-              color: Colors.pink,
-              child: Column(
-                
-                children: <Widget>[
-                Container(
-                  width: 200,
-                  height: 100,
-                  color: Colors.teal,
-                  child: Center(
-                    child: Text('Кубик первый', style: TextStyle(color: Colors.white, fontSize: 20, ),),
-                  ) 
-                ),
-                SizedBox(height: 20, ),
-                Container(
-                  width: 200,
-                  height: 100,
-                  color: Colors.yellow[200],
-                  child: Center(
-                    child: Text('Кубик второй', style: TextStyle(color: Colors.black, fontSize: 20, ),
-                ),
+              height: 100,
+              color: Colors.red,
+              child: FlatButton(
+                onPressed: null,
+                child: Text(
+                  'КИДАЙ',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
                   ),
                 ),
-              
-              ],
               ),
-              // decoration: BoxDecoration(
-              //   border: Border.all(
-              //     color: Colors.teal,
-              //     width: 10,
-              //   ),
-              //   // borderRadius:BorderRadius.circular(12),
-              // ),
-            ),),
             ),
-            SizedBox(height: 15 ,),
-          Container(
-            
-            width: 400,
-            height: 100,
-            color: Colors.red,
-            child: FlatButton(onPressed: null, child: Text('КИДАЙ', style: TextStyle(color:Colors.white, fontSize: 30, ),),),
-            
-          )
+            borderRadius: BorderRadius.circular(60),
+          ),
         ],
       ),
     );
