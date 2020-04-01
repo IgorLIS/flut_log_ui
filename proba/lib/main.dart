efa9c7bd45da7fbcefa226f3167bea9ab83c316f
IgorLIS <lapshin.is84@gmail.com>
2020-04-01 15:48:48 +0300

    1

--- proba/lib/main.dart
+++ proba/lib/main.dart
@@ -3,13 +3,12 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -20,7 +19,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-
   final String title;
 
   @override
@@ -29,26 +27,23 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _one;
 
   void _incrementCounter() {
     setState(() {
-      
-      _counter++;
+      // _counter++;
+      var one = _one;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        
         title: Text(widget.title),
       ),
       body: Center(
-        
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -58,17 +53,44 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
-          SizedBox(height: 15,),
-
-          TextField(
-            obscureText: true,
-            style: TextStyle(
-              color: Colors.red,
-              fontFamily: 'OpenSans',
+            Text(
+              '$one',
             ),
-            
-          ),
-          
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              padding: EdgeInsets.all(40),
+              child: TextField(
+                obscureText: false,
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontFamily: 'OpenSans',
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              padding: EdgeInsets.all(40),
+              child: TextFormField(
+                decoration: InputDecoration(labelText: 'Numbers'),
+                maxLength: 10,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Name is Required';
+                  }
+
+                  return null;
+                },
+                onSaved: (String value) {
+                  _one = value;
+                  print(_one) ;
+                },
+              ),
+            ),
           ],
         ),
       ),
@@ -76,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
